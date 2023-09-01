a7e6b6f67c481b86f8e0a139638abdbd22d682ab
Tushar Kant Nag <82863403+tushar-1202@users.noreply.github.com>
2023-09-01 16:01:45 +0530

    Update telnet_device.py

--- telnet_device.py
+++ telnet_device.py
@@ -5,11 +5,13 @@ import time
 HOST = "10.10.0.100"
 PORT = "2007"
 print("Inside")
+
 tn = telnetlib.Telnet(HOST, PORT)
 command = "cat /version.txt"
+tn.write(b"\n")
+response = tn.read_until(b'root@telekom:~#', timeout=10).decode('utf-8')
 tn.write(command.encode('utf-8') + b"\n")
-
-response = tn.read_until(b'root@telekom:~#').decode('utf-8')
+response = tn.read_until(b'root@telekom:~#', timeout=10).decode('utf-8')
 print(response)
 
 tn.close()
